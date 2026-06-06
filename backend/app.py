30ea795c59fe229d3d75765d13d217ec1b8a8141
UzielMedina22 <uziel.jonathan22@gmail.com>
2026-06-05 20:55:32 -0700

    Actualizar instrucciones del charbot

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, HTTPException
 from models.prompt_model import Prompt
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from db.vector_db import products_retriever
+from db.vector_db import products_retriever, promotions_retriever, faqs_retriever, advisors_retriever
+import datetime
 
 
 # ________________________________________________________________________________________
@@ -13,6 +14,7 @@ Este se descarga aparte con el comando "ollama pull llama3.2".
 """
 model = OllamaLLM(model="llama3.2")
 
+date = datetime.datetime.now()
 
 instructions = """
 Eres un asistente virtual de atención al cliente de la empresa internacional de muebles IKEA.
@@ -21,15 +23,36 @@ En caso de que algunas necesidades del clientes estén fuera de tu alcance, debe
 agente humano para que atienda al cliente, enviándole una notificación con la información con base en
 el contexto.
 
-Aquí hay productos {products} y ofertas {offers} relevantes.
+Hoy es {date}.
+
+Aquí hay productos {products} y ofertas {promotions} relevantes.
+
+Los FAQs son los siguientes: {faqs}
+
+Los asesores disponibles son: {advisors}
 
 Aquí está la pregunta del cliente: {question}
 
-Trata que las respuestas sean claras, breves y concisas.
+El historial de la conversación es este: {chat_history}. IMPORTANTE: Solo es para que lo leas y te bases en eso
+para tus repuestas. No lo menciones. Solo es como tu memoria \"cerebral\".
+
+También, presta ATENCIÓN: {greeting_instruction}
+
+URGENTE: Que las respuestas sean claras, breves, concisas y lo más actualizadas posible.
 """
 
 template_prompt = ChatPromptTemplate.from_template(instructions)
 chain = template_prompt | model
+chat_history = []
+
+# Formatear el historial del chat.
+def format_chat_history(history):
+    if not history:
+        return ""
+    return "\n\n".join(
+        f"Usuario: {item['Usuario']}\nAsistente: {item['content']}"
+        for item in history
+    )
 
 # _______________________________________________________________________________________
 
@@ -58,17 +81,40 @@ def health():
 # Endpoint para obtener la respuesta del chatbot.
 @API.post("/chatbot-conversation")
 async def chatbot_conversation(prompt: Prompt):
+    global chat_history
+    global date
+
+    date = datetime.datetime.now()
+    
     if not prompt.description or prompt.description.strip() == "":
         raise HTTPException(status_code=400, detail="El prompt ingresado es inválido o está vacío.")
 
     processed_prompt = prompt.description.strip()
+    
+    is_first_message = len(chat_history) == 0
+    
+    if is_first_message:
+        greeting_instruction = "Saluda al usuario en la primera interacción y responde su pregunta."
+    else:
+        greeting_instruction = "No saludes al usuario. Ve directo con la pregunta."
+    
     productos = products_retriever.invoke(processed_prompt)   # Obtener productos relevantes para el prompt.
+    promociones = promotions_retriever.invoke(processed_prompt)   # Obtener promociones relevantes para el prompt.
+    faqs = faqs_retriever.invoke(processed_prompt)   # Obtener FAQs relevantes para el prompt.
+    advisors = advisors_retriever.invoke(processed_prompt)   # Obtener asesores relevantes para el prompt.
 
     response = chain.invoke({
+        "date": date,
         "products": productos,
-        "offers": [],
-        "question": processed_prompt
+        "promotions": promociones,
+        "faqs": faqs,
+        "advisors": advisors,
+        "question": processed_prompt,
+        "chat_history": format_chat_history(chat_history),
+        "greeting_instruction": greeting_instruction
     })
+
+    chat_history.append({"Usuario": processed_prompt, "content": response})
 
     return {
         "user_prompt": processed_prompt,
--- backend/db/vector_db.py
+++ backend/db/vector_db.py
@@ -7,39 +7,155 @@ import pandas as pd
 # Embedding de Ollama en español e inglés.
 embedding = OllamaEmbeddings(model="jina/jina-embeddings-v2-base-es")
 
-products_dataframe = pd.read_csv("./db/seed/productos.csv")        # Importar productos desde el CSV.
-db_location = "./db_chroma"                                     # Directorio de la base de datos.
-must_add_product_documents = not os.path.exists(db_location)    # Verificar si hay base de datos.
+# Importar productos, FAQs, asesores y promociones desde sus respectivos archivos CSV.
+products_dataframe = pd.read_csv("./db/seed/productos.csv")
+faqs_dataframe = pd.read_csv("./db/seed/faq.csv")
+advisors_dataframe = pd.read_csv("./db/seed/asesores.csv")
+promotions_dataframe = pd.read_csv("./db/seed/promociones.csv")
+
+db_location = "./db_chroma"                                # Directorio de la base de datos.
+must_add_db_documents = not os.path.exists(db_location)    # Verificar si la base de datos ya existe.
 
 
-# Poblar la base de datos con los productos en el CSV.
-if must_add_product_documents:
-    product_documents = []
-    product_ids = []
+# IDs de los documentos según sus entidades.
+product_ids = []
+advisor_ids = []
+faq_ids = []
+promotion_ids = []
 
-    for id, row in products_dataframe.iterrows():
-        product_ids.append(id)
-        product_name = row["nombre"]
-        product_price = row["precio"]
-        product_category = row["categoria"]
-        product_desc = row["descripcion"]
-
-        product_info = f"Nombre: {product_name}\nPrecio: {product_price}\nCategoría: {product_category}\nDescripción: {product_desc}"
-        product_documents.append(Document(page_content=product_info))
+# Registros de cada entidad.
+product_documents = []
+advisor_documents = []
+faq_documents = []
+promotion_documents = []
 
 
-# Crear una colección de productos la base de datos.
+# Productos.
+for id, row in products_dataframe.iterrows():
+    product_ids.append(id)
+    product_id_key = row["product_id"]
+    product_line = row["product_line"]
+    product_name = row["product_name"]
+    product_category = row["category"]
+    product_color = row["color"]
+    product_width = row["width_cm"]
+    product_depth = row["depth_cm"]
+    product_height = row["height_cm"]
+    product_weight = row["weight_kg"]
+    product_price = row["price_mxn"]
+    product_stock_units = row["stock_units"]
+    product_availability = row["availability_status"]
+    product_source_url = row["source_url"]
+
+    product_info = f"ID: {product_id_key}\n" \
+                   f"Línea: {product_line}\n" \
+                   f"Nombre: {product_name}\n" \
+                   f"Categoría: {product_category}\n" \
+                   f"Color: {product_color}\n" \
+                   f"Ancho: {product_width} cm\n" \
+                   f"Profundidad: {product_depth} cm\n" \
+                   f"Alto: {product_height} cm\n" \
+                   f"Peso: {product_weight} kg\n" \
+                   f"Precio: ${product_price:.2f}\n" \
+                   f"Unidades en stock: {product_stock_units}\n" \
+                   f"Disponibilidad: {product_availability}\n" \
+                   f"URL de origen: {product_source_url}"
+
+    product_documents.append(Document(page_content=product_info))
+
+
+# FAQs.
+for id, row in faqs_dataframe.iterrows():
+    faq_ids.append(id)
+    faq_id_key = row["faq_id"]
+    faq_topic = row["topic"]
+    faq_question = row["question"]
+    faq_answer = row["answer"]
+
+    faq_info = f"ID: {faq_id_key}\n" \
+                f"Tópico: {faq_topic}\n" \
+                f"Pregunta: {faq_question}\n" \
+                f"Respuesta: {faq_answer}"
+
+    faq_documents.append(Document(page_content=faq_info))
+
+
+# Asesores.
+for id, row in advisors_dataframe.iterrows():
+    advisor_ids.append(id)
+    advisor_id_key = row["advisor_id"]
+    advisor_name = row["advisor_name"]
+    advisor_specialty = row["specialty"]
+    advisor_status = row["status"]
+    advisor_shift_start = row["shift_start"]
+    advisor_shift_end = row["shift_end"]
+
+    advisor_info = f"ID: {advisor_id_key}\n" \
+                   f"Nombre: {advisor_name}\n" \
+                   f"Especialidad: {advisor_specialty}\n" \
+                   f"Estado: {advisor_status}\n" \
+                   f"Inicio de turno: {advisor_shift_start}\n" \
+                   f"Fin de turno: {advisor_shift_end}"
+
+    advisor_documents.append(Document(page_content=advisor_info))
+
+
+# Promociones
+for id, row in promotions_dataframe.iterrows():
+    promotion_ids.append(id)
+    promotion_id_key = row["promotion_id"]
+    promotion_name = row["promotion_name"]
+    promotion_discount = row["discount_percent"]
+    promotion_start_date = row["start_date"]
+    promotion_end_date = row["end_date"]
+    promotion_status = row["promotion_status"]
+
+    promotion_info = f"ID: {promotion_id_key}\n" \
+                     f"Nombre: {promotion_name}\n" \
+                     f"Descuento: {promotion_discount}%\n" \
+                     f"Fecha de inicio: {promotion_start_date}\n" \
+                     f"Fecha de fin: {promotion_end_date}\n" \
+                     f"Estado: {promotion_status}"
+
+    promotion_documents.append(Document(page_content=promotion_info))
+
+
+# Crear colecciones de productos, asesores, FAQs y promociones en la base de datos.
 products_vector_store = Chroma(
     collection_name="productos",
     embedding_function=embedding,
     persist_directory=db_location
 )
 
+advisors_vector_store = Chroma(
+    collection_name="asesores",
+    embedding_function=embedding,
+    persist_directory=db_location
+)
 
-# Añadir productos.
-if must_add_product_documents:
-    products_vector_store.add_documents(product_documents, ids=product_ids)
+faq_vector_store = Chroma(
+    collection_name="faqs",
+    embedding_function=embedding,
+    persist_directory=db_location
+)
+
+promotions_vector_store = Chroma(
+    collection_name="promociones",
+    embedding_function=embedding,
+    persist_directory=db_location
+)
 
 
-# Buscar productos por cantidad (k).
-products_retriever = products_vector_store.as_retriever(search_kwargs={"k": 10})   # Buscar documentos.
+# Añadir productos, FAQs, asesores y promociones a sus respectivas colecciones.
+if must_add_db_documents:
+    products_vector_store.add_documents(product_documents, ids=[str(id) for id in product_ids])
+    advisors_vector_store.add_documents(advisor_documents, ids=[str(id) for id in advisor_ids])
+    faq_vector_store.add_documents(faq_documents, ids=[str(id) for id in faq_ids])
+    promotions_vector_store.add_documents(promotion_documents, ids=[str(id) for id in promotion_ids])
+
+
+# Buscar productos, FAQs, asesores y promociones por cantidad (k).
+products_retriever = products_vector_store.as_retriever(search_kwargs={"k": 10})
+advisors_retriever = advisors_vector_store.as_retriever(search_kwargs={"k": 10})
+faqs_retriever = faq_vector_store.as_retriever(search_kwargs={"k": 10})
+promotions_retriever = promotions_vector_store.as_retriever(search_kwargs={"k": 10})
